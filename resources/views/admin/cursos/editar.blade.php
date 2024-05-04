c74781ad5c7d9376aec313266d7c16d48720c65e
DiogoHHarada <diogo.harada@unesp.br>
2024-05-04 09:16:40 -0300

    criação do adicionar e editar.blade e seus métodos

---
+++ resources/views/admin/cursos/editar.blade.php
@@ -0,0 +1,14 @@
+@extends('layout.site')
+@section('titulo','Cursos')
+@section('conteudo')
+    <div class="container">
+        <h3 class="center">Editando Curso</h3>
+        <div class="row">
+            <form class="" action="{{route('admin\curso\atualizar', $linha->id)}}" method="post" enctype="multipart/form-data">
+                {{ csrf_field() }}
+                @include('admin.cursos._form')
+                <button class="btn deep-orange">Atualizar</button>
+            </form>
+        </div>
+    </div>
+@endsection
