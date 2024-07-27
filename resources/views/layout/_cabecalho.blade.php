5572ce79ef64ad6d98f7eb8619213995285585ee
DiogoHHarada <diogo.harada@unesp.br>
2024-07-27 09:28:00 -0300

    login e home

--- resources/views/layout/_cabecalho.blade.php
+++ resources/views/layout/_cabecalho.blade.php
@@ -18,12 +18,23 @@
                 <a href="#" data-target="mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
                     <ul class="right hide-on-med-and-down">
                         <li><a href="/">Home</a></li>
-                        <li><a href="{{route('admin.cursos')}}">Cursos</a></li>
+                        @if(Auth::guest()) <!-- se guest() retorna true -- eh pq o usuário não esta logado -->
+                            <li><a href="{{route('site.login')}}">Login</a></li>
+                            @else <!-- se o teste no método 'entrar' do controller retornou true ... -->
+                            <li><a href="{{route('admin.cursos')}}">Cursos</a></li>
+                            <li><a href="#">{{Auth::user()->name}}</a></li>
+                            <li><a href="{{ route('site.login.sair') }}">Sair</a></li>
+                        @endif
                     </ul>
             </div>
         </nav>
 
         <ul class="sidenav" id="mobile">
-        <li><a href="/">Home</a></li>
-        <li><a href="{{route('admin.cursos')}}">Cursos</a></li>
+            @if(Auth::guest()) <!-- se guest() retorna true -- eh pq o usuário não esta logado -->
+                <li><a href="{{route('site.login')}}">Login</a></li>
+                @else <!-- se o teste no método 'entrar' do controller retornou true ... -->
+                <li><a href="{{route('admin.cursos')}}">Cursos</a></li>
+                <li><a href="#">{{Auth::user()->name}}</a></li>
+                <li><a href="{{ route('site.login.sair') }}">Sair</a></li>
+            @endif
         </ul>
--- routes/web.php
+++ routes/web.php
@@ -2,31 +2,37 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::group({'middleware'=>'auth'},function(){
+    Route::get('/admin/cursos',
+    ['as' =>'admin.cursos',
+    'uses'=>'App\Http\Controllers\Admin\CursoController@index']);
 
-Route::get('/admin/cursos',
-['as' =>'admin.cursos',
-'uses'=>'App\Http\Controllers\Admin\CursoController@index']);
+    Route::get('/admin/cursos/adicionar',
+    ['as' =>'admin.cursos.adicionar',
+    'uses'=>'App\Http\Controllers\Admin\CursoController@adicionar']);
 
-Route::get('/admin/cursos/adicionar',
-['as' =>'admin.cursos.adicionar',
-'uses'=>'App\Http\Controllers\Admin\CursoController@adicionar']);
+    Route::put('/admin/cursos/salvar',
+    ['as' =>'admin.cursos.salvar',
+    'uses'=>'App\Http\Controllers\Admin\CursoController@salvar']);
 
-Route::put('/admin/cursos/salvar',
-['as' =>'admin.cursos.salvar',
-'uses'=>'App\Http\Controllers\Admin\CursoController@salvar']);
+    Route::get('/admin/cursos/editar/{id}',
+    ['as' =>'admin.cursos.editar',
+    'uses'=>'App\Http\Controllers\Admin\CursoController@editar']);
 
-Route::get('/admin/cursos/editar/{id}',
-['as' =>'admin.cursos.editar',
-'uses'=>'App\Http\Controllers\Admin\CursoController@editar']);
+    Route::put('/admin/cursos/atualizar/{id}',
+    ['as' =>'admin.cursos.atualizar',
+    'uses'=>'App\Http\Controllers\Admin\CursoController@atualizar']);
 
-Route::put('/admin/cursos/atualizar/{id}',
-['as' =>'admin.cursos.atualizar',
-'uses'=>'App\Http\Controllers\Admin\CursoController@atualizar']);
+    Route::get('/admin/cursos/excluir/{id}',
+    ['as' =>'admin.cursos.excluir',
+    'uses'=>'App\Http\Controllers\Admin\CursoController@excluir']);
+//});
 
-Route::get('/admin/cursos/excluir/{id}',
-['as' =>'admin.cursos.excluir',
-'uses'=>'App\Http\Controllers\Admin\CursoController@excluir']);
+Route::get('/login', ['as' => 'site.login',
+'uses'=>'App\Http\Controllers\Site\LoginController@index']);
 
+Route::post('/login/entrar',['as'=>'site.login.entrar',
+'uses'=>'App\Http\Controllers\Site\LoginController@entrar']);
+
+Route::get('/login/sair',['as'=>'site.login.sair',
+'uses'=>'App\Http\Controllers\Site\LoginController@sair']);
